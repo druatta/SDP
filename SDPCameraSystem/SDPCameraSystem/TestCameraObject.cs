4e0337958e39fc031b74ca848abd6ef4e9dfaa86
druatta <druatta@ucsc.edu>
2017-01-24 22:00:03 -0800

    Attempt 2 - SDP
    New attempt to try to get Visual Studio to compile

---
+++ SDPCameraSystem/SDPCameraSystem/TestCameraObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DALSA.SaperaLT.SapClassBasic;
+using System.Diagnostics;
+
+namespace SDPCameraSystem
+{
+    class TestCameraObject
+    {
+        /* All test driven development is done in main(). Every unit test returns a boolean and has
+         * the word Test in front of it. It is then tested using Trace.Assert() and will output an 
+         * error message if the assertion (and therefore the unit test) fails, giving us immediate info
+         * on which unit test failed.
+         */ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Camera object's unit tests!");
+            Trace.Assert(TestCreateCamera(), "Failed to create camera!"); 
+
+
+            while(true);
+        }
+
+        static Boolean TestCreateCamera()
+        {
+            try
+            {
+
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+        } //end TestCreateCamera
+        
+       
+    }//end TestCameraObject
+}
